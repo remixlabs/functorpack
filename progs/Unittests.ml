f29788752bce09c444608c0fd44b245695db01f0
Gerd Stolpmann <gerd@figlyinc.com>
2023-08-28 17:24:17 +0200

    tests

--- progs/Unittests.ml
+++ progs/Unittests.ml
@@ -1,7 +1,12 @@
 open FPack
 open T_frame.Util
 
-let test001() =
+type 'a compose_extract =
+  { compose : Yojson.Safe.json -> 'a;
+    extract : 'a -> Yojson.Safe.json
+  }
+
+let test001 ce () =
   let j =
     `Assoc [ "key1", `Null;
              "key2", `Bool false;
@@ -20,39 +25,39 @@ let test001() =
              "key15", `String (String.make 200000 'X');
              "key16", `Float 3.14
            ] in
-  let by = Yojson.Safe.compose_bytes j in
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
+  let by = ce.compose j in
+  let j' = ce.extract by in
   j = j'
 
-let test002() =
+let test002 ce () =
   let j = `Assoc [] in
-  let by = Yojson.Safe.compose_bytes j in
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
+  let by = ce.compose j in
+  let j' = ce.extract by in
   j = j'
 
-let test003() =
+let test003 ce () =
   let j =
     `Assoc [ "c", `Int 7;
              "d", `Int 5;
              "b", `Int 6;
              "a", `Int 10
            ] in
-  let by = Yojson.Safe.compose_bytes j in
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
+  let by = ce.compose j in
+  let j' = ce.extract by in
   j = j'
 
-let test004() =
+let test004 ce () =
   let j =
     `Assoc [ "a", `List [ `Int 56; `String "abc"; `Null ];
              "b", `List [ `Null; `String "abc"; `Int 56; ];
              "c", `List (Array.to_list (Array.make 20000 (`Int 42)));
              "d", `List (Array.to_list (Array.make 200000 (`Int 42)));
            ] in
-  let by = Yojson.Safe.compose_bytes j in
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
+  let by = ce.compose j in
+  let j' = ce.extract by in
   j = j'
 
-let test005() =
+let test005 ce () =
   let l =
     [ "key4", `Intlit "5";
       "key5", `Intlit "-5";
@@ -66,35 +71,66 @@ let test005() =
   let j = `Assoc l in
   let j1 =
     `Assoc (List.map (fun (s, `Intlit n) -> (s, `Int (int_of_string n))) l) in
-  let by = Yojson.Safe.compose_bytes j in
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
+  let by = ce.compose j in
+  let j' = ce.extract by in
   j1 = j'
 
-let test006() =
+let test006 ce () =
   let j = `Intlit "9223372036854775807" in  (* = Int64.max_int *)
-  let by = Yojson.Safe.compose_bytes j in
+  let by = ce.compose j in
+  let j' = ce.extract by in
 (*
   for i = 0 to Bytes.length by - 1 do
     Printf.printf "by[%d] = %d\n%!" i (Char.code (Bytes.get by i))
   done;
  *)
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
   j = j'
 
-let test007() =
+let test007 ce () =
   let j = `Intlit "-9223372036854775808" in  (* = Int64.min_int *)
-  let by = Yojson.Safe.compose_bytes j in
-  let j' = Yojson.Safe.extract_bytes by 0 (Bytes.length by) in
+  let by = ce.compose j in
+  let j' = ce.extract by in
   j = j'
 
+let ce_bytes =
+  { compose = Yojson.Safe.compose_bytes;
+    extract = (fun s -> Yojson.Safe.extract_bytes s 0 (Bytes.length s));
+  }
+
+let ce_rope =
+  { compose = Yojson.Safe.compose_rope;
+    extract = (fun s -> Yojson.Safe.extract_rope s 0 (Rope.length s));
+  }
+
+let ce_big =
+  { compose = Yojson.Safe.compose_big;
+    extract = (fun s -> Yojson.Safe.extract_big s 0 (Bigarray.Array1.dim s));
+  }
+  
 let tests =
-  [ "test001", test001;
-    "test002", test002;
-    "test003", test003;
-    "test004", test004;
-    "test005", test005;
-    "test006", test006;
-    "test007", test007;
+  [ "test001", test001 ce_bytes;
+    "test002", test002 ce_bytes;
+    "test003", test003 ce_bytes;
+    "test004", test004 ce_bytes;
+    "test005", test005 ce_bytes;
+    "test006", test006 ce_bytes;
+    "test007", test007 ce_bytes;
+
+    "test101", test001 ce_rope;
+    "test102", test002 ce_rope;
+    "test103", test003 ce_rope;
+    "test104", test004 ce_rope;
+    "test105", test005 ce_rope;
+    "test106", test006 ce_rope;
+    "test107", test007 ce_rope;
+
+    "test201", test001 ce_big;
+    "test202", test002 ce_big;
+    "test203", test003 ce_big;
+    "test204", test004 ce_big;
+    "test205", test005 ce_big;
+    "test206", test006 ce_big;
+    "test207", test007 ce_big;
   ]
 
 let () =
